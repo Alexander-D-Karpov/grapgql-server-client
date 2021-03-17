c326903903596b4ca0737dbd63c3d01893107e07
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2021-03-17 15:35:37 +0300

    added conn check

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import socket
+from server_data import server_ip, server_port
 
 sock = socket.socket()
-sock.connect((server_ip, 4040))
-
+sock.connect((server_ip, 7777))
+sock.send('Ok'.encode())
+data = sock.recv(1024).decode()
+print(data)
 sock.close()
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 4040))
+sock.bind(('', 7777))
 sock.listen(1)
 
 while True:
     conn, addr = sock.accept()
     print('connected:', addr)
-    data = conn.recv(1024)
+    data = conn.recv(1024).decode()
+    print(data)
+    conn.send('Ok'.encode())
+    conn.close()
