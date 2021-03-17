e68b166d949fbec738e31ca7fbb5179067e0afc8
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2021-03-17 15:35:19 +0300

    added ip/port file ad con check

--- client.py
+++ client.py
@@ -1,26 +1,6 @@
-from python_graphql_client import GraphqlClient
+import socket
 
-# Instantiate the client with an endpoint.
-client = GraphqlClient(endpoint="https://countries.trevorblades.com")
+sock = socket.socket()
+sock.connect((server_ip, 4040))
 
-# Create the query string and variables required for the request.
-query = """
-    query countryQuery($countryCode: String) {
-        country(code:$countryCode) {
-            code
-            name
-        }
-    }
-"""
-variables = {"countryCode": "CA"}
-
-# Synchronous request
-data = client.execute(query=query, variables=variables)
-print(data)  # => {'data': {'country': {'code': 'CA', 'name': 'Canada'}}}
-
-
-# Asynchronous request
-import asyncio
-
-data = asyncio.run(client.execute_async(query=query, variables=variables))
-print(data)  # => {'data': {'country': {'code': 'CA', 'name': 'Canada'}}}   
+sock.close()
--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-from python_graphql_client import GraphqlClient
+import socket
+
+sock = socket.socket()
+sock.bind(('', 4040))
+sock.listen(1)
+
+while True:
+    conn, addr = sock.accept()
+    print('connected:', addr)
+    data = conn.recv(1024)
